fb71019bd3a32b0afffb8d079932d489f385de38
ricardodametto <ricardo.dametto@gmail.com>
2026-06-18 23:51:23 -0300

    commit apos flake update

--- Gnome-Environment/gnome-essentials.nix
+++ Gnome-Environment/gnome-essentials.nix
@@ -23,6 +23,7 @@
     enable = true;
     nix-direnv.enable = true;
     enableZshIntegration = true;
+    enableFishIntegration = true; 
   };
 
   # --- SSH gráfico (GNOME keyring / SSH agent) ---
